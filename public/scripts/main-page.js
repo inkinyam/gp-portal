82bd660d63e34dcf1bcfacc650e3899c20f74afe
inki <inkinyam@yandex.ru>
2023-06-02 12:34:31 +0300

    fix: конфетти

--- public/scripts/main-page.js
+++ public/scripts/main-page.js
@@ -11,3 +11,30 @@ let sliders = document.querySelectorAll('.js-carousel');
 sliders.forEach(item => {
   new Carousel(item, options);
 })
+
+
+let birthdayCards = document.querySelectorAll('.birthdays__item');
+birthdayCards.forEach((item, indx) => {
+ let inputs = item.querySelectorAll('.birthdays__input');
+ let labels = item.querySelectorAll('.birthdays__element');
+
+ inputs.forEach((input, number) => {
+  input.setAttribute('id', 'radio'+indx+number);
+  input.setAttribute('name', 'smiles'+indx);
+ })
+
+ labels.forEach((label, number) => {
+   label.setAttribute('for','radio'+indx+number);
+   
+   label.addEventListener('click', (e)=> {
+    e.preventDefault;
+    label.classList.remove('animate');
+    label.classList.add('animate');
+    setTimeout(function(){
+      label.classList.remove('animate');
+    },700);
+   }, false)
+ })
+
+})
+
